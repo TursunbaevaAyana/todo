cd83901bcdf5c576246981cc2f596f362c0b3435
Ayana <ayanaturrsunbaeva@gmail.com>
2020-04-26 13:10:52 +0600

    Create ToDo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'; 
 import './App.css';
+import ToDo from './containers/ToDo/ToDo';
 
 function App() {
   return (
     <div className="App"> 
+    <ToDo />
     </div>
   );
 }
