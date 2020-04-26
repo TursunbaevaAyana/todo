e97918740c3d5dc8cff768edd0cf2258a45158c2
Ayana <ayanaturrsunbaeva@gmail.com>
2020-04-26 13:54:45 +0600

    Add button component

--- src/components/Items/Item/Item.js
+++ src/components/Items/Item/Item.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import classes from "./Item.module.css";
+import Button from "../../UI/Button/Button";
 
 export default ({ item }) => (
     <li className={classes.Item}>
         <span className={classes.text}>{item.text}</span>
+        <Button red>x</Button>
     </li>
 );
---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,6 @@
+import React, { Children } from "react";
+import classes from "./Button.module.css";
+
+export default ({ children }) => (
+    <button className={classes.Button}>{children}</button>
+);
