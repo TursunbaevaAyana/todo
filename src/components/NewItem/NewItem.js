8725f034dba0f218ade3260cb95953eced6827f7
Ayana <ayanaturrsunbaeva@gmail.com>
2020-05-03 15:17:19 +0600

    Add feature

--- src/components/NewItem/NewItem.js
+++ src/components/NewItem/NewItem.js
@@ -2,9 +2,21 @@ import React, { Children } from "react";
 import classes from "./NewItem.module.css";
 import Button from "../UI/Button/Button";
 
-export default ({}) => (
+export default ({ addItem }) => {
+function addButtonClicked({ target }) {
+  const input = target.previousSibling;
+  
+  addItem(input.value);
+  input.value = "";
+  input.focus();
+}
+
+  return (
     <div className={classes.NewItem}>
-        <input type="text"/>
-        <Button green>Add</Button>
+      <input type="text" />
+      <Button click={addButtonClicked} green>
+        Add
+      </Button>
     </div>
-);
+  );
+};
--- src/containers/ToDo/ToDo.js
+++ src/containers/ToDo/ToDo.js
@@ -27,9 +27,16 @@ export default () => {
        setItems(newItems);
     }
 
+    function addItem(text) {
+       const newItems = {...items};
+       const id = `todo-${Math.floor(Math.random() * 10000)}`;
+       newItems[id] = { text, completed: false,};
+       setItems(newItems);
+    }
+
     return (
         <div className={classes.ToDo}>
-            <NewItem />
+            <NewItem addItem={addItem}/>
             <Items items={items} deleteItem={deleteItem} toggleCompleteItem={toggleCompleteItem}/>
         </div>
     );
